8c5f7e481720207797573f930551a3efd5d6ae68
kaurluhaaar <kaur.luhaaar@ametikool.ee>
2022-04-01 09:03:36 +0300

    cum

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'database' => [
+        'host' => 'd101915.mysql.zonevs.eu',
+        'db' => 'd101915_tak20todo',
+        'user' => 'd101915_todotak2',
+        'pass' => 'suemaonlahe',
+        'charset' => 'UTF8MB4',
+    ]
+];
